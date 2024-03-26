616eb5ea4c4f52534fee731b92a815e0dbe3c240
Tailong Cheng <tailong.cheng@dcmail.ca>
2024-03-26 19:16:07 -0400

    Update2

--- main.tf
+++ main.tf
@@ -119,7 +119,7 @@ resource "google_compute_instance" "default" {
   metadata {
     startup-script = <<SCRIPT
     sudo apt-get update -y
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release git -y
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
       "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
